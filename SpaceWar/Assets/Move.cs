1614d3c09391dd0b285b66f058679a3afd2d7f30
sgarden1204 <dkswjddnjs0@naver.com>
2018-03-19 20:46:14 +0900

    Script
    move

---
+++ SpaceWar/Assets/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+
+    CharacterController controller;
+    Vector3 move;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float ver = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+
+        move = new Vector3(hor * moveSpeed, ver * moveSpeed, 0);
+
+        controller.Move(move * Time.deltaTime);
+	}
+}
