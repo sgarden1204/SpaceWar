e7df5dc9f331c76f540c92723549b3cc4089fd74
sgarden1204 <dkswjddnjs0@naver.com>
2018-03-21 23:35:23 +0900

    Script & Sound Asset
    MissileMove Script
    PlayerFire Script

---
+++ SpaceWar/Assets/MissileMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileMove : MonoBehaviour {
+
+    public float moveSpeed = 15.0f;
+
+    private void Update()
+    {
+        transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+    }
+}
---
+++ SpaceWar/Assets/PlayerFire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour {
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate = 0.1f;
+
+    private float nextFire;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Z) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
