4910078f25f23379b6d0e1b9422612d96c3556f8
sgarden1204 <dkswjddnjs0@naver.com>
2018-03-21 21:37:44 +0900

    Script
    Asteroid
    Destroy Asteroid

---
+++ SpaceWar/Assets/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+    public float moveSpeed = -10.0f;
+    public float rot = 45.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.forward * rot * Time.deltaTime);
+	}
+}
---
+++ SpaceWar/Assets/DestroyAsteroid.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAsteroid : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit Trigger!");
+
+        Destroy(this.gameObject);
+    }
+}
