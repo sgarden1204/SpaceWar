1ad8da6ac01ac2348d6675e04bbba3fe7692a515
sgarden1204 <dkswjddnjs0@naver.com>
2018-03-26 23:49:18 +0900

    20180326
    Main Scene Edit
    Main Audio Asset Import
    Font Asset Import
    
    AudioManager Script
    MainManager Script
    Volume Script

---
+++ SpaceWar/Assets/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    static AudioManager audioInstance = null;
+
+    public static AudioManager Instance()
+    {
+        return audioInstance;
+    }
+
+    private void Start()
+    {
+        if(audioInstance == null)
+        {
+            audioInstance = this;
+        }
+    }
+
+    public void PlayClip(AudioClip clip)
+    {
+        GetComponent<AudioSource>().PlayOneShot(clip);
+    }
+
+    public void StopClip(AudioClip clip)
+    {
+        GetComponent<AudioSource>().Stop();
+    }
+}
---
+++ SpaceWar/Assets/MainManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor.UI;
+
+public class MainManager : MonoBehaviour {
+
+    public AudioClip clip;
+
+    public Transform optionPanel;
+
+    private bool active = false;
+
+    private void Start()
+    {
+        AudioManager.Instance().PlayClip(clip);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Sysnopsis");
+        //Application.LoadLevel(1);
+    }
+
+    public void StartModeGame()
+    {
+        //SceneManager.LoadScene();
+    }
+
+    public void ActiveOption()
+    {
+        if(active)
+        {
+            optionPanel.gameObject.SetActive(false);
+            active = false;
+        }
+
+        else
+        {
+            optionPanel.gameObject.SetActive(true);
+            active = true;
+        }
+    }
+}
