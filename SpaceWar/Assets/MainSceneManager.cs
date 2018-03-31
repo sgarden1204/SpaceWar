a51397eb3f221358cd4ed0f1723f476145d880aa
sgarden1204 <dkswjddnjs0@naver.com>
2018-03-31 18:13:19 +0900

    Main Scene
    Redesign

---
+++ SpaceWar/Assets/MainSceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneManager : MonoBehaviour
+{
+
+    public AudioClip clip;
+
+    private void Start()
+    {
+        AudioManager.Instance().PlayClip(clip);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Sysnopsis");
+    }
+
+    public void StartModeGame()
+    {
+        //SceneManager.LoadScene();
+    }
+}
---
+++ SpaceWar/Assets/UIManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public static UIManager uiInstatnce = null;
+
+    private bool active = false;
+
+    public Transform optionPanel;
+
+    Slider volumeSlider;
+
+    public static UIManager Instance()
+    {
+        return uiInstatnce;
+    }
+
+    private void Start()
+    {
+        if(uiInstatnce != null)
+        {
+            Destroy(this.gameObject);
+        }
+
+        else
+        {
+            uiInstatnce = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void ActiveOption()
+    {
+        if (active)
+        {
+            optionPanel.gameObject.SetActive(false);
+            active = false;
+        }
+
+        else
+        {
+            optionPanel.gameObject.SetActive(true);
+            active = true;
+        }
+    }
+
+    public void FullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void ChangeSlider()
+    {
+        volumeSlider = GameObject.Find("Volume Slider").GetComponent<Slider>();
+        Volume.volumeOption = volumeSlider.value;
+    }
+}
