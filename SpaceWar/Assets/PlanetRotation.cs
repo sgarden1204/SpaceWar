03b38d0a5d7197a73d1872f96ec17be3c0fb0627
sgarden1204 <dkswjddnjs0@naver.com>
2018-04-08 04:31:56 +0900

    Asset Import
    UI, Crosshair

---
+++ SpaceWar/Assets/PlanetRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour {
+
+    public float angle = 1.0f;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        this.transform.Rotate(Vector3.up, angle);
+	}
+}
