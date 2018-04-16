ea380e79e3146d2f8f644911e235817d0bd8e630
sgarden1204 <dkswjddnjs0@naver.com>
2018-04-16 14:56:03 +0900

    Stage2 Scene
    BGM Add

---
+++ SpaceWar/Assets/Stage2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage2 : MonoBehaviour {
+
+    public AudioClip clip;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<AudioSource>().PlayOneShot(clip, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
