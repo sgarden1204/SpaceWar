3844950c3947f5ce75d54f51d8613fa3e1bec24f
sgarden1204 <dkswjddnjs0@naver.com>
2018-04-16 09:24:50 +0900

    Tutorial UI Scene
    Complete

--- SpaceWar/Assets/MyScripts/TutorialText.cs
+++ SpaceWar/Assets/MyScripts/TutorialText.cs
@@ -5,6 +5,24 @@ using UnityEngine.UI;
 
 public class TutorialText : MonoBehaviour {
 
+    /// <summary>
+    /// 
+    /// 
+    /// 
+    /// </summary>
+
+    public Slider lc;
+    public Slider shield;
+    public Button pause;
+    public Transform pausePanel;
+    public Text score;
+    public Text scoreText;
+    public Image bullet;
+    public Image scope;
+    public Image crosshair;
+    public Image change;
+
+
     public Text[] text = new Text[15];
 
     private int next = 0;
@@ -29,13 +47,57 @@ public class TutorialText : MonoBehaviour {
 
         switch (next)
         {
-            case 7:
+            case 3:
+                pause.gameObject.SetActive(true);
+                //pausePanel.gameObject.SetActive(true);
+                break;
+            case 4:
+                pause.gameObject.SetActive(false);
+                //pausePanel.gameObject.SetActive(false);
 
-                image.sprite = sprite;
+                score.gameObject.SetActive(true);
+                scoreText.gameObject.SetActive(true);
                 break;
 
+            case 5:
+                score.gameObject.SetActive(false);
+                scoreText.gameObject.SetActive(false);
+
+                bullet.gameObject.SetActive(true);
+                break;
+
+            case 7:
+                bullet.gameObject.SetActive(false);
+
+                change.gameObject.SetActive(true);
+                break;
+
+            case 8:
+                change.gameObject.SetActive(false);
+
+                shield.gameObject.SetActive(true);
+                break;
+
+            case 10:
+                shield.gameObject.SetActive(false);
+
+                lc.gameObject.SetActive(true);
+                break;
+
+            case 13:
+                lc.gameObject.SetActive(true);
+                shield.gameObject.SetActive(true);
+                pause.gameObject.SetActive(true);
+                //pausePanel.gameObject.SetActive(true);
+                score.gameObject.SetActive(true);
+                scoreText.gameObject.SetActive(true);
+                bullet.gameObject.SetActive(true);
+                //scope.gameObject.SetActive(true);
+                //crosshair.gameObject.SetActive(true);
+                change.gameObject.SetActive(true);
+                break;
             case 15:
-                panel.gameObject.SetActive(false);
+                //panel.gameObject.SetActive(false);
                 CancelInvoke();
                 break;
             default:
