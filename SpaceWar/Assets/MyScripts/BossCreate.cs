63d7082a428940ba9c6bc46d86746e162cf8fda8
sgarden1204 <dkswjddnjs0@naver.com>
2018-04-16 13:05:59 +0900

    Tutorial FireAndMove
    Editing...

---
+++ SpaceWar/Assets/MyScripts/BossCreate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossCreate : MonoBehaviour {
+
+    public GameObject sideBoss;
+    public Slider bossSlider;
+    public Transform player;
+
+    public float distance = 10.0f;
+
+    private GameObject boss;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            bossSlider.gameObject.SetActive(true);
+            boss = Instantiate(sideBoss, player.position, player.rotation);
+            boss.transform.position = new Vector3(player.position.x, player.position.y, player.position.z + distance);
+            boss.transform.Rotate(0.0f, -180.0f, 0.0f);
+        }
+    }
+}
---
+++ SpaceWar/Assets/MyScripts/TutorialFireandMove.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialFireandMove : MonoBehaviour
+{
+    public GameObject redAsteroid;
+    public GameObject blueAsteroid;
+    public GameObject rockAsteroid;
+
+    public Text[] goal = new Text[3];
+
+    public Text[] uiux = new Text[3];
+
+
+    public Text[] text = new Text[15];
+
+    private int next = 0;
+
+    public Image image;
+    public Sprite sprite;
+    public Transform panel;
+
+    void Start()
+    {
+        InvokeRepeating("StartText", 2.5f, 2.5f);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void StartText()
+    {
+        text[next].gameObject.SetActive(false);
+        next++;
+        text[next].gameObject.SetActive(true);
+
+        switch (next)
+        {
+            case 6:
+                redAsteroid.gameObject.SetActive(true);
+                blueAsteroid.gameObject.SetActive(true);
+                rockAsteroid.gameObject.SetActive(true);
+
+                break;
+
+            case 7:
+                goal[0].gameObject.SetActive(true);
+                goal[1].gameObject.SetActive(true);
+                goal[2].gameObject.SetActive(true);
+                break;
+
+            case 12:
+                uiux[0].gameObject.SetActive(true);
+                uiux[1].gameObject.SetActive(true);
+                uiux[2].gameObject.SetActive(true);
+
+                break;
+
+            case 15:
+                panel.gameObject.SetActive(false);
+                CancelInvoke();
+                break;
+            default:
+                break;
+        }
+    }
+}
