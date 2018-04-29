dfcd510a23cf5dd659f047674d8499083b4d865a
sgarden1204 <dkswjddnjs0@naver.com>
2018-04-29 14:32:59 +0900

    Stage3 Prototype
    Top View

---
+++ SpaceWar/Assets/TopViewBossMoveAndRotation.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopViewBossMoveAndRotation : MonoBehaviour {
+
+    public float rotationSpeed = 1.0f;
+    public float moveSpeed = 1.0f;
+
+    float currentPos;
+
+    bool leftmove = true;
+    bool rightmove = false;
+
+	// Use this for initialization
+	void Start () {
+
+        currentPos = this.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Rotate(0.0f,0.0f, 1.0f * rotationSpeed * Time.deltaTime);
+
+
+        if (leftmove)
+        {
+            if (this.transform.position.x >= -20.0f)
+            {
+                this.transform.transform.position = new Vector3(currentPos, this.transform.position.y, this.transform.position.z);
+                currentPos -= moveSpeed * Time.deltaTime;
+            }
+
+            else
+            {
+                leftmove = false;
+                rightmove = true;
+            }
+        }
+
+        if (rightmove)
+        {
+            if (this.transform.position.x <= 20.0f)
+            {
+                this.transform.transform.position = new Vector3(currentPos, this.transform.position.y, this.transform.position.z);
+                currentPos += moveSpeed * Time.deltaTime;
+            }
+
+            else
+            {
+                leftmove = true;
+                rightmove = false;
+            }
+        }
+    }
+}
---
+++ SpaceWar/Assets/TopViewPlayerMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopViewPlayerMove : MonoBehaviour {
+
+    public float moveSpeed = 10.0f;
+    public Slider shield;
+
+    CharacterController player;
+    Vector3 move;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float ver = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+
+        move = new Vector3(hor * moveSpeed, 0.0f, ver * moveSpeed);
+
+        player.Move(move * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wall" || other.tag == "Enemy")
+        {
+            shield.value -= 10;
+        }
+    }
+}
