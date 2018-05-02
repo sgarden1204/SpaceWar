97e92dce8454ab4eb3b81f519852ac07107a3b6a
sgarden1204 <dkswjddnjs0@naver.com>
2018-05-02 23:14:53 +0900

    Stage4Start Scene
    Prototype

---
+++ SpaceWar/Assets/TotalViewLeftBossAni.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotalViewLeftBossAni : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.x <= -10)
+        {
+            this.transform.position = new Vector3(this.transform.position.x + moveSpeed * Time.deltaTime, this.transform.position.y, this.transform.position.z);
+        }
+	}
+}
