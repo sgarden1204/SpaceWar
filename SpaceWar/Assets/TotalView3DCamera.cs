43fcf054ea3af2e8d0c2a1b7af63b48ac08be901
sgarden1204 <dkswjddnjs0@naver.com>
2018-05-04 01:31:57 +0900

    Stage4 Edit
    Player and Boss

---
+++ SpaceWar/Assets/TotalView3DCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotalView3DCamera : MonoBehaviour {
+
+    public Transform playerPos;
+    public float yPos = 1.0f;
+    public float xPos = 1.0f;
+    public float zPos = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        this.transform.position = new Vector3(playerPos.position.x + xPos, playerPos.position.y + yPos, playerPos.position.z + zPos);
+        this.transform.LookAt(playerPos);
+	}
+}
