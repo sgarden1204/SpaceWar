f81408c1f31526f794e74e10bca605ed73d3490b
sgarden1204 <dkswjddnjs0@naver.com>
2018-05-07 04:25:34 +0900

    Stage4End Scene
    Prototype

---
+++ SpaceWar/Assets/EndGameLeftShipAni.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameLeftShipAni : MonoBehaviour {
+
+    public float moveSpeed = 1.0f;
+
+    public GameObject Laser;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.z < 10)
+        {
+            this.transform.position = new Vector3(this.transform.position.x + moveSpeed * Time.deltaTime, this.transform.position.y, this.transform.position.z + moveSpeed * Time.deltaTime);
+        }
+
+        else
+        {
+            Instantiate(Laser);
+        }
+	}
+}
---
+++ SpaceWar/Assets/EndGameRightShipAni.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameRightShipAni : MonoBehaviour {
+
+    public float moveSpeed = 10.0f;
+
+    public GameObject Laser;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z < 10)
+        {
+            this.transform.position = new Vector3(this.transform.position.x - moveSpeed * Time.deltaTime, this.transform.position.y, this.transform.position.z + moveSpeed * Time.deltaTime);
+        }
+
+        else
+        {
+            Instantiate(Laser);
+        }
+    }
+}
