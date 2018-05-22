8619ece41b8f310242153092fae0db873b43e35f
sgarden1204 <dkswjddnjs0@naver.com>
2018-05-23 03:54:19 +0900

    Stage3 Scene
    Upgrade Complete

--- SpaceWar/Assets/MyScripts/TopViewPlayerMove.cs
+++ SpaceWar/Assets/MyScripts/TopViewPlayerMove.cs
@@ -8,6 +8,18 @@ public class TopViewPlayerMove : MonoBehaviour {
 
     public float moveSpeed = 10.0f;
     public Slider shield;
+    public GameObject hitSpark;
+    public GameObject itemCollect;
+    public GameObject playerDestroy;
+
+    public AudioClip sparkCilp;
+    public AudioClip playerHitClip;
+    public AudioClip MineExplosion;
+
+    public AudioClip shieldCilp;
+    public AudioClip lCClip;
+    public AudioClip scoreClip;
+    public AudioClip powerUpClip;
 
     CharacterController player;
     Vector3 move;
@@ -28,6 +40,13 @@ public class TopViewPlayerMove : MonoBehaviour {
         move = new Vector3(hor * moveSpeed, 0.0f, ver * moveSpeed);
 
         player.Move(move * Time.deltaTime);
+
+        if(this.transform.position.x <= -20.0f || this.transform.position.x >= 20.0f || this.transform.position.z >= 30.0f || this.transform.position.z <= -2.0f)
+        {
+            shield.value--;
+            Instantiate(hitSpark, this.transform.position, this.transform.rotation);
+            GetComponent<AudioSource>().PlayOneShot(sparkCilp);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,12 +54,60 @@ public class TopViewPlayerMove : MonoBehaviour {
         if (other.tag == "Asteroid" || other.tag == "Mine" || other.tag == "EnemyBullet")
         {
             shield.value -= 2;
+            EZCameraShake.CameraShaker.Instance.ShakeOnce(2.5f, 2.5f, 1.0f, 1.0f);
+            GetComponent<AudioSource>().PlayOneShot(playerHitClip);
 
-            if(shield.value <= 0)
+            if(other.tag == "Mine")
+                GetComponent<AudioSource>().PlayOneShot(MineExplosion,0.5f);
+
+            if (shield.value <= 0)
             {
-                ScoreManager.Instance().ScoreSave();
-                SceneManager.LoadScene("Result");
+                Instantiate(playerDestroy,this.transform.position,this.transform.rotation);
+                Invoke("GameOver", 2.5f);
             }
         }
+
+        if(other.tag == "ItemPowerUp")
+        {
+            GetComponent<AudioSource>().PlayOneShot(powerUpClip);
+            Instantiate(itemCollect, this.transform.position, this.transform.rotation);
+            
+        }
+
+        if (other.tag == "LCItem")
+        {
+            GetComponent<AudioSource>().PlayOneShot(lCClip);
+        }
+
+        if (other.tag == "ShieldItem")
+        {
+            GetComponent<AudioSource>().PlayOneShot(shieldCilp, 1.0f);
+            Instantiate(itemCollect, this.transform.position, this.transform.rotation);
+        }
+
+        if (other.tag == "ScoreItem")
+        {
+            GetComponent<AudioSource>().PlayOneShot(scoreClip);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Core")
+        {
+            shield.value--;
+            Instantiate(hitSpark, this.transform.position, this.transform.rotation);
+            GetComponent<AudioSource>().PlayOneShot(sparkCilp);
+        }
+    }
+
+    public void GameOver()
+    {
+        if (ScoreManager.Instance() != null)
+        {
+            ScoreManager.Instance().ScoreSave();
+        }
+
+        SceneManager.LoadScene("Result");
     }
 }
